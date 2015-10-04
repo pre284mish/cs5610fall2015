1cd27298b4e339783a86936201b80540de63a700
preety <preety@ccs.neu.edu>
2015-10-04 13:10:05 -0700

    changed server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/public'));
 
-app.listen(3000);
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+app.listen(port, ipaddress);
