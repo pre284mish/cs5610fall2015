72a82d7f1e970eed6068d35873653b8d4d71414a
preety <preety@ccs.neu.edu>
2015-12-08 21:09:35 -0800

    Commit for server-openshift

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ var bodyParser = require('body-parser');
 
 
 var mongoose = require('mongoose');
-console.log("requireMongoose: " + mongoose);
-mongoose.connect('mongodb://localhost/cs5610Project');
-var db = mongoose.connection;
+//mongoose.connect('mongodb://localhost/cs5610Assignment');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,7 +15,20 @@ app.use(express.static(__dirname + '/public'));
 var ipaddress 	= process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port 		= process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-require("./public/project/server/app.js")(app,db,mongoose);
+
+var connectionString = 'mongodb://127.0.0.1:27017/cs5610';
+
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+var db = mongoose.connect(connectionString);
+
+require("./public/assignment/server/app.js")(app,db,mongoose);
 //require("./public/assignment/server/services/user.service.js")(app);
 //require("./public/assignment/server/services/form.service.js")(app);
 //require("./public/assignment/server/services/field.service.js")(app);
