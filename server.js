0ba8ea8636785354aadf067a854af10622d972ea
preety <preety@ccs.neu.edu>
2015-12-08 22:14:47 -0800

    Server to DB

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var ipaddress 	= process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port 		= process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 
-var connectionString = 'mongodb://127.0.0.1:27017/cs5610';
+var connectionString = 'mongodb://127.0.0.1:27017/cs5610Project';
 
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
