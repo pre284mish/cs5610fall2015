428fd4175af2f4d94df390c7d6ef4339c53c5d4e
preety <preety@ccs.neu.edu>
2015-12-10 13:50:11 -0800

    Commit for listings

--- public/project/client/views/my_listings/my_listings.controller.js
+++ public/project/client/views/my_listings/my_listings.controller.js
@@ -18,8 +18,11 @@
             console.log("get all jobs of userId:" + userId);
             JobService.findAllJobsByUserId(userId)
                 .then(function(jobs){
+                if(jobs == ''){
+                    model.jobs = '';
+                }
                 console.log("All the jobs:"+JSON.stringify(jobs, null, 4));
-                console.log("address::"+ JSON.stringify(jobs[0].address.street, null, 4));
+//                console.log("address::"+ JSON.stringify(jobs[0].address.street, null, 4));
                 model.jobs = jobs;
             });
         }
